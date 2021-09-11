350f5ddfc962e198656f2e247ad3083dc4997a38
Arpit Mudgal <arpit.sharma1630@gmail.com>
2021-09-11 14:36:16 +0530

    String Calculator

--- StringCalculatorTestCases.java
+++ StringCalculatorTestCases.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class StringCalculatorShould {
+public class StringCalculatorTestCases {
     @Test
     public void
     return_0_when_input_is_empty() {
